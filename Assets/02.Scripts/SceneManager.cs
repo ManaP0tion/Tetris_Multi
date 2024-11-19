4d1b432c22df11afa88968a3578ab9faf52908f5
ManaP0tion <aer0dev7@gmail.com>
2024-11-19 13:51:21 +0900

    Cilent Initial Commit

---
+++ Assets/02.Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake(){
+        Screen.SetResolution(1920, 1080, true);
+    }
+}
