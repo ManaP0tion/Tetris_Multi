4e4d29363e3526c8ac658c83f033d73ea236d6c1
Aer0dev <45550827+ManaP0tion@users.noreply.github.com>
2024-11-27 15:21:38 +0900

    add Movement

--- Client/Assets/Scripts/Piece.cs
+++ Client/Assets/Scripts/Piece.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Board board{ get; private set; }
+    public TetrominoData data{ get; private set; }
+    public Vector3Int[] cells{ get; private set; }
+    public Vector3Int position{ get; private set; }
+    
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
-        
+        this.board = board;
+        this.position = position;
+        this.data = data;
+
+        if(this.cells == null){
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+
+        for(int i = 0; i< data.cells.Length; i++){
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        this.board.Clear(this);
+
+        if(Input.GetKeyDown(KeyCode.A )){
+            Move(Vector2Int.left);
+            Debug.Log("left arrow");
+        }
+        else if (Input.GetKeyDown(KeyCode.D )){
+            Move(Vector2Int.right);
+            Debug.Log("right arrow");
+        }
+        this.board.Set(this);
+    }
+
+    private bool Move(Vector2Int translation){
+        Vector3Int newPos = this.position;
+        newPos.x += translation.x;
+        newPos.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, newPos);
+
+        if(valid){
+            this.position = newPos;
+        }
+        return valid;
     }
 }
